516efdef8a73a20ca4fc8d8831efe92bf5fae200
petra.pylkki <petra.pylkki@hotmail.com>
2020-01-24 21:36:44 +0200

    testing

---
+++ src/control/Esimerkki2.java
@@ -0,0 +1,21 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/")
+public class Esimerkki2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Git esimerkki, joka löytyy tiedostohakemistosta ").append(request.getContextPath());
+	}
+
+
+}
