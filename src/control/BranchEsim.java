84c62d23efc110b561d13b0bdf9cbe826f4a735a
petra.pylkki <petra.pylkki@hotmail.com>
2020-01-28 15:02:17 +0200

    test1 commit

---
+++ src/control/BranchEsim.java
@@ -0,0 +1,18 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/haara")
+public class BranchEsim extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("<h1>jee uusi haara lol</h1>");
+	}
+}
